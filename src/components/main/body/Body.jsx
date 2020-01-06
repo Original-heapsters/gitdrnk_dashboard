e6c3813f411caa949255a1887a6a714bedaad74d
sellnat77 <sell_nat@yahoo.com>
2020-01-05 22:39:43 -0800

    Added some basic bulma layout

---
+++ src/components/main/body/Body.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Body = () => (
+    <div className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-one-third has-background-primary">
+          Players + Games
+          </div>
+          <div className="column is-two-thirds has-background-danger">
+          Chat
+          </div>
+        </div>
+      </div>
+    </div>
+);
+
+export default Body;
