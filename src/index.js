54cdfccaaf213c0e215ae7bce07c52c6f139658a
sellnat77 <sell_nat@yahoo.com>
2020-02-09 11:55:49 -0800

    Getting started reduxifying

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
 import 'bulma';
 
 
 var mountNode = document.getElementById("root");
-ReactDOM.render(<App name="Janey" />, mountNode);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  mountNode
+);
